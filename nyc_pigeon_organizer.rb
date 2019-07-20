3bc796db45eead043ba9053b69c6588411bd1f70
Kimberly Bone <k_bone@uncg.edu>
2019-07-20 06:27:15 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -1,27 +1,5 @@
 def nyc_pigeon_organizer(data)
-# Type your Ruby code below!
-pigeon_data = {
-  :color => {
-    :purple => ["Theo", "Peter Jr.", "Lucky"],
-    :grey => ["Theo", "Peter Jr.", "Ms. K"],
-    :white => ["Queenie", "Andrew", "Ms. K", "Alex"],
-    :brown => ["Queenie", "Alex"]
-  },
-  :gender => {
-    :male => ["Alex", "Theo", "Peter Jr.", "Andrew", "Lucky"],
-    :female => ["Queenie", "Ms. K"]
-  },
-  :lives => {
-    "Subway" => ["Theo", "Queenie"],
-    "Central Park" => ["Alex", "Ms. K", "Lucky"],
-    "Library" => ["Peter Jr."],
-    "City Hall" => ["Andrew"]
-  }
-}
-
-def pigeon_organizer (data)
   new = {}
-  array = []
   data.each do |key, value|
     value.each do |k,v|
       v.each do |name|
@@ -57,7 +35,6 @@ def pigeon_organizer (data)
       end
     end
   end
-  puts new
+  return new
 end 
-pigeon_organizer(pigeon_data)
-end
+
